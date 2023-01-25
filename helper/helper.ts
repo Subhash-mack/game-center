d1eeed2fa08286c6c8fbd1349129b43582ba57a4
Subhash <subhash@qburst.com>
2023-01-25 18:01:09 +0530

    [NT] logic change in analytics

--- helper/helper.ts
+++ helper/helper.ts
@@ -5,49 +5,59 @@ const calculateTotalSession = (sessions: session[]) => {
     return acc + val.session
   }, 0)
 }
-export const getGames = (analytics: analytics) => {
+export const getGames = (analytics: analytics[]) => {
   const games: { [key: string]: number } = {}
-  for (const user in analytics) {
-    analytics[user].map(game => {
-      const gameData = Object.values(game)[0]
-      const gameName = Object.keys(game)[0]
-      if (!games[gameName]) {
-        games[gameName] = calculateTotalSession(gameData)
-      } else {
-        games[gameName] += calculateTotalSession(gameData)
-      }
-    })
+  for (const analytic of analytics) {
+    const analyticsData = analytic["analytics"]
+    for (const user in analyticsData) {
+      analyticsData[user].map(game => {
+        const gameData = Object.values(game)[0]
+        const gameName = Object.keys(game)[0]
+        if (!games[gameName]) {
+          games[gameName] = calculateTotalSession(gameData)
+        } else {
+          games[gameName] += calculateTotalSession(gameData)
+        }
+      })
+    }
   }
   return games
 }
 
-export const getPlayers = (analytics: analytics) => {
+export const getPlayers = (analytics: analytics[]) => {
   const players: { [key: string]: number } = {}
-  for (const user in analytics) {
-    analytics[user].map(game => {
-      const gameData = Object.values(game)[0]
-      if (!players[user]) {
-        players[user] = calculateTotalSession(gameData)
-      } else {
-        players[user] += calculateTotalSession(gameData)
-      }
-    })
+  for (const analytic of analytics) {
+    const analyticsData = analytic["analytics"]
+    for (const user in analyticsData) {
+      analyticsData[user].map(game => {
+        const gameData = Object.values(game)[0]
+        if (!players[user]) {
+          players[user] = calculateTotalSession(gameData)
+        } else {
+          players[user] += calculateTotalSession(gameData)
+        }
+      })
+    }
   }
+
   return players
 }
 
-export const getGameAnalytics = (gameName: string, analytics: analytics) => {
+export const getGameAnalytics = (gameName: string, analytics: analytics[]) => {
   const graph: { [key: string]: number } = {}
-  for (const user in analytics) {
-    analytics[user].map(game => {
-      if (gameName === Object.keys(game)[0]) {
-        const gameData = Object.values(game)[0]
-        gameData.forEach(({ date, session }) => {
-          if (!graph[date]) graph[date] = session
-          else graph[date] += session
-        })
-      }
-    })
+  for (const analytic of analytics) {
+    const analyticsData = analytic["analytics"]
+    for (const user in analyticsData) {
+      analyticsData[user].map(game => {
+        if (gameName === Object.keys(game)[0]) {
+          const gameData = Object.values(game)[0]
+          gameData.forEach(({ date, session }) => {
+            if (!graph[date]) graph[date] = session
+            else graph[date] += session
+          })
+        }
+      })
+    }
   }
   return graph
 }
